f653e3ff62a411e618c11a0a8fa38d8748bcecab
Andrei Vlad Dome <andreivlad.dome@gmail.com>
2025-02-10 09:37:45 +0100

    Move recursive subgraph enum alg to motif feat file

--- feature_engineering/motif_features.py
+++ feature_engineering/motif_features.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import itertools
 
 
 def node_match(node1, node2):
@@ -79,3 +80,59 @@ def count_node_induced_subgraph_isomorphisms(G, SG, use_v2=False):
     # TODO: Return the nodes/edges in G that are isomorphic to SG
 
     return count, unique_mappings
+
+
+### Naive slow subgraph enumeration algorihtm ###
+
+def extract_all_motifs_in_graph(G, min_num_nodes_in_motif, max_num_nodes_in_motif):
+    nodes = list(G.nodes())
+    all_motifs = []
+    for k in range(min_num_nodes_in_motif, max_num_nodes_in_motif + 1):
+        print(k)
+        for subset in itertools.combinations(nodes, k):
+            SG = G.subgraph(subset)  # Induced subgraph
+            if nx.is_connected(SG):  # Must be connected (No isolated nodes)
+                all_motifs.append(SG)
+    return all_motifs
+
+
+### Faster subgraph enumeration algorithm ###
+ 
+def recursive_local_expand(G, node_set, possible, excluded, results, max_size):
+        """
+        Recursive function to add an extra node to the subgraph being formed
+        """
+        results.append(node_set)
+        if len(node_set) == max_size:  # Should this be max_size + 1?
+            return
+        for node in possible - excluded:
+            new_node_set = node_set | {node}
+            excluded = excluded | {node}
+            new_possible = (possible | set(G.neighbors(node))) - excluded
+            recursive_local_expand(G, new_node_set, new_possible, excluded, results, max_size)
+
+
+def get_all_connected_subgraphs(G, min_motif_size, max_motif_size):
+    """Get all connected subgraphs by a recursive procedure"""
+
+    # Connected components sorted in decreasing order by number of nodes
+    con_comp = [c for c in sorted(nx.connected_components(G), key=len, reverse=True)]  
+   
+    results = []
+    for cc in con_comp:
+        max_size = max_motif_size  #len(cc)
+
+        excluded = set()
+        for node in G:
+            excluded.add(node)
+            node_set = {node}
+            possible = set(G.neighbors(node)) - excluded
+            recursive_local_expand(G, node_set, possible, excluded, results, max_size)
+
+    results.sort(key=len)
+
+    results_no_single_nodes = [motif for motif in results if len(motif) > 1]
+
+    results_nx_objects = [G.subgraph(motif) for motif in results_no_single_nodes]
+
+    return results_no_single_nodes, results_nx_objects
